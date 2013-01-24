78f68941c09ffe2b4953fb03abf6b538850fb981
andriy <samsoncan@gmail.com>
2013-01-24 00:28:12 -0500

    Create basic skeleton for application (may be modified later)

---
+++ Kingdom/test/Example.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author andriy
+ */
+public class Example {
+    
+    public Example() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
