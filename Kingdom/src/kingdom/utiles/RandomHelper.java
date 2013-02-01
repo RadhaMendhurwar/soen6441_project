d282e16d0d1347af570f6a8dabbe093158df0fdf
andriy <samson@gmail.com>
2013-02-01 14:20:43 -0500

    Create a helper class that have any required functionality to work with Random number. Actually implemented methods to generate random number and to SHUFFLE any existing list

---
+++ Kingdom/src/kingdom/utiles/RandomHelper.java
@@ -0,0 +1,47 @@
+package kingdom.utiles;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * contains all functionality to work with random numbers
+ */
+public final class RandomHelper {
+
+    private RandomHelper(){
+        // not suppose to be instantiated
+    }
+    
+    /**
+     * Generate random number between <code>0</code> ... and <code>max</code>
+     * @param max Top limit (Excluded) of number to generate
+     * @return random number from range [0...max-1)
+     */
+    public static int getRandom(int max) {
+        Random randGen = new Random();
+        return randGen.nextInt(max);
+    }
+
+    
+    /**
+     * Shuffle provided list <b>howManyTimes</b> switches.
+     * @param <T> Type of list
+     * @param list List to be shuffled
+     * @param howManyTimes Number of shuffling in the list
+     */
+    public static <T> void shuffleList(List<T> list, int howManyTimes) {
+        Random randGen = new Random();
+        int SIZE = list.size();
+        T temp;
+        int left;
+        int right;
+
+        for (int i = 0; i < howManyTimes; ++i) {
+            left = randGen.nextInt(SIZE);
+            right = randGen.nextInt(SIZE);
+            temp = list.get(left);
+            temp = list.set(right, temp);
+            list.set(left, temp);
+        }
+    }
+}
