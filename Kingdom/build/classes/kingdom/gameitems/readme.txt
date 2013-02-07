8069fa10c1897fc13e9a24f4460868348ea09404
varunrayen <varunrayen@gmail.com>
2013-02-07 14:29:55 -0500

    Implementation for invoking Text mode of the game

---
+++ Kingdom/build/classes/kingdom/gameitems/readme.txt
@@ -0,0 +1,6 @@
+The package suppose contain all classes that represents physical items in the came:
+- tile
+- user
+- coin
+- castle
+- ....
--- Kingdom/src/kingdom/Main.java
+++ Kingdom/src/kingdom/Main.java
@@ -3,6 +3,7 @@
  */
 package kingdom;
 
+import java.util.Scanner;
 import javax.swing.SwingUtilities;
 import kingdom.ui.MainWindow;
 import kingdom.ui.StartWizard;
@@ -16,14 +17,47 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+          int mode;
+        Scanner modeScan = new Scanner(System.in);
         System.out.println("This is the start point of Application\n");
-        SwingUtilities.invokeLater(new Runnable() {
-
-            public void run() {
-                MainWindow mWindow = new MainWindow();
-                mWindow.setVisible(true);
-            }
-        });
+        System.out.println("Select 1.GUI mode \n 2.Text Mode");
+        mode = modeScan.nextInt();
+        Main mainobj = new Main();
+        switch(mode)
+        {
+            
+            case 1:
+               mainobj.GUI();
+            break;
+        
+            case 2:
+               mainobj.textMode();
+             break;
+       }
+        
+        
+    
         
     }
+    /**
+ * Method to invoke GUI mode of the game
+ */
+    public void GUI()
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                MainWindow mWindow = new MainWindow();
+                mWindow.setVisible(true);
+                    }
+                });
+    }
+        /**
+ * Method to invoke Text mode of the game
+ */
+    
+    public void textMode()
+    {
+        System.out.println("Welcome to Kingdoms - Text Mode");
+    }
+    
 }
